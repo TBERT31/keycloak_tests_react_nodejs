673f922ac87426d5bfca4d6654cf2c6d837f3d3f
TBERT31 <berteau.thomas@hotmail.fr>
2025-01-29 18:07:07 +0100

    Only front react

---
+++ demokeycloak-react/src/App.jsx
@@ -0,0 +1,12 @@
+import Protected from "./components/Protected";
+import Public from "./components/Public";
+
+import useAuth from "./hooks/useAuth";
+
+function App() {
+  const isLogin = useAuth();
+
+  return isLogin ? <Protected/> : <Public/>;
+}
+
+export default App;
