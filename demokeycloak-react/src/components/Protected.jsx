5ee0d8da9bafb86068a34f83e7a899144c8a03c2
TBERT31 <berteau.thomas@hotmail.fr>
2025-01-29 20:27:55 +0100

    push react and nodejs app

--- demokeycloak-react/src/components/Protected.jsx
+++ demokeycloak-react/src/components/Protected.jsx
@@ -1,7 +1,37 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
-const Protected = () =>{
-    return <div>Protected</div>
-}
+const Protected = ({ token }) => {
+  const isRun = useRef(false);
 
-export default Protected
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (isRun.current) return;
+
+    isRun.current = true;
+
+    const config = {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    };
+
+    axios
+      .get("/documents", config)
+      .then((res) => setData(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return data ? (
+    <>
+      {data.map((rec, i) => (
+        <h3 key={i}>{rec}</h3>
+      ))}
+    </>
+  ) : (
+    <div>Protected</div>
+  );
+};
+
+export default Protected;
--- demokeycloak-react/vite.config.js
+++ demokeycloak-react/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/documents": "http://localhost:5000/",
+      "/documents": "http://localhost:5173/",
+    },
+  },
   plugins: [react()],
-})
+});
